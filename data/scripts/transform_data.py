2afe3c8f82d9e7ae88b177e058f2a53baf35d242
S-Rombach <Sebastian.Rombach@t-online.de>
2025-08-27 17:38:49 +0200

    normalize power values to peak power

--- data/scripts/transform_data.py
+++ data/scripts/transform_data.py
@@ -9,7 +9,6 @@ import pandas as pd
 from src.config import (
     DATA_RAW_DIR,
     DATA_ORIG_DIR,
-    DATA_ORIG_FILENAME,
     DATA_RAW_FILENAME,
 )
 
@@ -38,14 +37,44 @@ column_translations = {
     "Wallbox Gesamtladeleistung": "wb_tot_charge",
 }
 
+power_columns = [
+    "bat_charge",
+    "bat_discharge",
+    "grid_feed_in",
+    "grid_cons",
+    "sol_prod_1",
+    "sol_prod_2",
+    "sol_prod_3",
+    "sol_prod",
+    "house_cons",
+    "add_cons",
+    "ext_cons",
+    "tot_cons",
+    "wb_0_tot_charge",
+    "wb_0_grid_cons",
+    "wb_0_sol_charge",
+    "wb_tot_charge",
+]
+"""The columns that are used for power measurements."""
+
 all_data = pd.DataFrame(
-    columns=column_translations.keys(),
+    columns=column_translations.values(),
     dtype=str,
+)
+
+peak_power_df = pd.read_csv(
+    os.path.join(DATA_ORIG_DIR, "peak_power_output.csv"),
+    sep=";",
+    index_col="installation",
 )
 
 for dir in os.listdir(DATA_ORIG_DIR):
     if not os.path.isdir(os.path.join(DATA_ORIG_DIR, dir)):
         continue
+
+    peak_power = peak_power_df.loc[dir, "Wp"]
+
+    print(f"Processing installation: {dir} with peak power {peak_power} W")
 
     for file in os.listdir(os.path.join(DATA_ORIG_DIR, dir)):
         if not file.endswith(".csv"):
@@ -58,12 +87,22 @@ for dir in os.listdir(DATA_ORIG_DIR):
         df = pd.read_csv(orig_file_path, sep=";", dtype=str)
 
         df["installation"] = dir
+        df = df.rename(columns=column_translations)
+
+        df["soc"] = df["soc"].apply(pd.to_numeric, errors="coerce").div(100)
+
+        this_power_columns = [col for col in power_columns if col in df.columns]
+
+        # normalize all power values as part of the peak power
+        # provides comparability across installations
+        # anonymize the installation owner further by not giving details about the installation
+        df[this_power_columns] = df[this_power_columns].apply(
+            pd.to_numeric, errors="coerce"
+        ).div(peak_power)
 
         # Append the data to the all_data DataFrame
         all_data = pd.concat([all_data, df], ignore_index=True, axis=0)
 
-
-all_data = all_data.rename(columns=column_translations)
 # After processing all files in the directory, save the combined data
 if not all_data.empty:
     all_data.to_csv(os.path.join(DATA_RAW_DIR, DATA_RAW_FILENAME), index=False, sep=";")
