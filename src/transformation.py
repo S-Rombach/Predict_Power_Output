57d18f0b1a8b926418fc1c13039ae0469c458f0b
S-Rombach <Sebastian.Rombach@t-online.de>
2025-09-30 12:10:17 +0200

    allow requests from past to present

--- src/transformation.py
+++ src/transformation.py
@@ -1,6 +1,8 @@
 """Custom data transformation functions used in different parts of the project."""
 
 from typing import List
+from datetime import datetime as dt
+import numpy as np
 import pandas as pd
 import requests_cache
 from retry_requests import retry
@@ -52,68 +54,103 @@ def fetch_openmeteo_weather_data(lat, lon, start_date, end_date, wc_codes=None):
     retry_session = retry(cache_session, retries=5, backoff_factor=0.2)
     openmeteo = openmeteo_requests.Client(session=retry_session)
 
-    start_date = pd.to_datetime(start_date).strftime("%Y-%m-%d")
-    end_date = pd.to_datetime(end_date).strftime("%Y-%m-%d")
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
 
     # Make sure all required weather variables are listed here
     # The order of variables in hourly or daily is important to assign them correctly below
-    url = "https://archive-api.open-meteo.com/v1/archive"
-    params = {
-        "latitude": lat,
-        "longitude": lon,
-        "start_date": start_date,
-        "end_date": end_date,
-        "hourly": [
-            "weather_code",
-            "cloud_cover",
-            "snow_depth",
-            "sunshine_duration",
-            "is_day",
-            "direct_radiation",
-        ],
-    }
+    hist_url = "https://archive-api.open-meteo.com/v1/archive"
+    curr_url = "https://api.open-meteo.com/v1/forecast"
 
-    responses = openmeteo.weather_api(url, params=params)
+    requests = []
+    responses = []
 
-    # Process first location. Add a for-loop for multiple locations or weather models
-    response = responses[0]
+    hourly_dataframe = None
 
-    # Process hourly data. The order of variables needs to be the same as requested.
-    hourly = response.Hourly()
-    hourly_weather_code = hourly.Variables(0).ValuesAsNumpy()
-    hourly_cloud_cover = hourly.Variables(1).ValuesAsNumpy()
-    hourly_snow_depth = hourly.Variables(2).ValuesAsNumpy()
-    hourly_sunshine_duration = hourly.Variables(3).ValuesAsNumpy()
-    hourly_is_day = hourly.Variables(4).ValuesAsNumpy()
-    hourly_direct_radiation = hourly.Variables(5).ValuesAsNumpy()
+    today = pd.to_datetime(dt.now().date())
 
-    hourly_data = {
-        "date": pd.date_range(
-            start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
-            end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
-            freq=pd.Timedelta(seconds=hourly.Interval()),
-            inclusive="left",
+    if start_date < today:
+        if end_date < today:
+            requests.append((hist_url, start_date, end_date))
+        else:
+            requests.append(
+                (hist_url, start_date, today - pd.Timedelta(days=1))
+            )
+            requests.append((curr_url, today, end_date))
+    else:
+        requests.append((curr_url, start_date, end_date))
+
+    for url, s_date, e_date in requests:
+        params = {
+            "latitude": lat,
+            "longitude": lon,
+            "start_date": s_date.strftime("%Y-%m-%d"),
+            "end_date": e_date.strftime("%Y-%m-%d"),
+            "hourly": [
+                "weather_code",
+                "cloud_cover",
+                "snow_depth",
+                "sunshine_duration",
+                "is_day",
+                "direct_radiation",
+            ],
+        }
+
+        resps = openmeteo.weather_api(url, params=params)
+
+        # Process first location. Add a for-loop for multiple locations or weather models
+        response = resps[0]
+        responses.append(response)
+
+        # Process hourly data. The order of variables needs to be the same as requested.
+        hourly = response.Hourly()
+        hourly_weather_code = hourly.Variables(0).ValuesAsNumpy()
+        hourly_cloud_cover = hourly.Variables(1).ValuesAsNumpy()
+        hourly_snow_depth = hourly.Variables(2).ValuesAsNumpy()
+        hourly_sunshine_duration = hourly.Variables(3).ValuesAsNumpy()
+        hourly_is_day = hourly.Variables(4).ValuesAsNumpy()
+        hourly_direct_radiation = hourly.Variables(5).ValuesAsNumpy()
+
+        hourly_data = {
+            "date": pd.date_range(
+                start=pd.to_datetime(hourly.Time(), unit="s", utc=True),
+                end=pd.to_datetime(hourly.TimeEnd(), unit="s", utc=True),
+                freq=pd.Timedelta(seconds=hourly.Interval()),
+                inclusive="left",
+            )
+        }
+
+        hourly_data["weather_code"] = hourly_weather_code
+        hourly_data["cloud_cover"] = np.nan_to_num(hourly_cloud_cover, nan=0.0)
+        hourly_data["snow_depth"] = np.nan_to_num(
+            hourly_snow_depth, nan=0.0
+        )  # das hier ist NAN, wie rettet man das?
+        hourly_data["sunshine_duration"] = np.nan_to_num(
+            hourly_sunshine_duration, nan=0.0
         )
-    }
+        hourly_data["is_day"] = hourly_is_day
+        hourly_data["direct_radiation"] = np.nan_to_num(
+            hourly_direct_radiation, nan=0.0
+        )
 
-    hourly_data["weather_code"] = hourly_weather_code
-    hourly_data["cloud_cover"] = hourly_cloud_cover
-    hourly_data["snow_depth"] = hourly_snow_depth
-    hourly_data["sunshine_duration"] = hourly_sunshine_duration
-    hourly_data["is_day"] = hourly_is_day
-    hourly_data["direct_radiation"] = hourly_direct_radiation
+        if wc_codes is not None:
+            hourly_data["weather_description"] = [
+                wc_codes.get(code, "Unknown") for code in hourly_weather_code
+            ]
 
-    if wc_codes is not None:
-        hourly_data["weather_description"] = [
-            wc_codes.get(code, "Unknown") for code in hourly_weather_code
-        ]
+        hourly_dataframe = (
+            pd.DataFrame(data=hourly_data)
+            if hourly_dataframe is None
+            else pd.concat(
+                [hourly_dataframe, pd.DataFrame(data=hourly_data)], ignore_index=True
+            )
+        )
 
-    hourly_dataframe = pd.DataFrame(data=hourly_data)
     return hourly_dataframe, {
-        "actual_lat": response.Latitude(),
-        "actual_lon": response.Longitude(),
-        "elevation": response.Elevation(),
-        "utc_offset_seconds": response.UtcOffsetSeconds(),
+        "actual_lat": responses[0].Latitude(),
+        "actual_lon": responses[0].Longitude(),
+        "elevation": responses[0].Elevation(),
+        "utc_offset_seconds": responses[0].UtcOffsetSeconds(),
     }
 
 
@@ -136,9 +173,6 @@ def one_hot_encode_weather_descriptions(
 
     df = df.copy()
     ohe = pd.get_dummies(df[weather_column])
-    ohe = ohe.rename(
-        columns={col: col.strip().lower().replace(" ", "_") for col in ohe.columns}
-    )
 
     if len(mandatory_weather_columns) > 0:
         ohe = ohe[[col for col in ohe.columns if col in mandatory_weather_columns]]
@@ -146,6 +180,10 @@ def one_hot_encode_weather_descriptions(
     for col in mandatory_weather_columns:
         if col not in ohe.columns:
             ohe[col] = 0
+
+    ohe = ohe.rename(
+        columns={col: col.strip().lower().replace(" ", "_") for col in ohe.columns}
+    )
 
     return ohe
 
